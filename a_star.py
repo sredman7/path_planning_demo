0083b542f0184c41bb9bc6eb65f6f39fbc9b3243
Sam Redman <cp1590@wayne.edu>
2025-11-25 12:03:45 -0500

    Basic A* and LPA* implementations, no replanning yet

---
+++ a_star.py
@@ -0,0 +1,196 @@
+import numpy as np
+import heapq
+import matplotlib.pyplot as plt
+
+# main A* algorithm
+def a_star(grid: np.ndarray, start_pos: tuple[int, int], goal_pos: tuple[int, int]) -> list[tuple[int,int]]:
+    '''
+    Use the A* path planning algorithm to return a list of nodes given
+    an occupancy grid of obstacles, a start node, and a goal node.
+
+    Keyword arguments:
+    grid -- 2D numpy array occupancy grid, 0 = free 1 = obstacle; note (row, col) = (y, x)
+    start -- integer pair coordinates (x,y) of start node on occupancy grid
+    goal -- integer pair coordinates (x,y) of goal node on occupancy grid
+
+    Returns:
+    Ordered list of node coordinates (x,y) on computed path from start to goal
+    '''
+
+    # create start node
+    start_node = create_node(
+        position = start_pos,
+        g = 0.0,
+        h = euclidean_distance(start_pos, goal_pos)
+    )
+
+    # initialize lists
+    open_list = [(start_node['f'], start_pos)] # priority queue of nodes to explore - positions ordered by estimated cost
+    open_dict = {start_pos: start_node} # dict of full node information, key is position
+    closed_set = set() # set of positions already reached, starts empty
+
+    # main loop
+    # while there are nodes to explore, check the one with the lowest estimated cost
+    while open_list:
+        # use heapq to pop lowest cost node from list
+        _, current_pos = heapq.heappop(open_list)
+        current_node = open_dict[current_pos]
+
+        # check for goal
+        if current_pos == goal_pos:
+            return construct_path(current_node)
+
+        # mark node as reached
+        closed_set.add(current_pos)
+
+        # explore neighbors
+        for neighbor_pos in get_neighbors(grid=grid, position=current_pos):
+            # skip if already reached
+            if neighbor_pos in closed_set:
+                continue
+
+            # calculate path cost 
+            g_new = current_node['g'] + euclidean_distance(current_pos, neighbor_pos)
+
+            # create or update adjacent node
+            if neighbor_pos not in open_dict:
+                # node not in queue, create it
+                neighbor_node = create_node(
+                    position = neighbor_pos,
+                    g = g_new,
+                    h = euclidean_distance(neighbor_pos, goal_pos),
+                    parent = current_node
+                )
+
+                # add to queue
+                heapq.heappush(open_list, (neighbor_node['f'], neighbor_pos))
+                open_dict[neighbor_pos] = neighbor_node
+
+            elif g_new < open_dict[neighbor_pos]['g']:
+                # better path to existing node, update path and total cost
+                neighbor_node = open_dict[neighbor_pos]
+                neighbor_node['g'] = g_new
+                neighbor_node['f'] = g_new + neighbor_node['h']
+                neighbor_node['parent'] = current_node
+
+    # if no path is found, return empty list
+    return []
+
+# helpers
+def create_node(position: tuple[int, int], g=float('inf'), h=0.0, parent=None) -> dict:
+    '''
+    Create a node with the given properties
+
+    Keyword arguments:
+    position -- integer pair coordinates (x,y) of node position on occupancy grid
+    g -- path cost from start; defaults to infinity
+    h -- Euclidean distance to goal heuristic; defaults to zero
+    parent -- parent node on exploration tree; defaults to none
+
+    Returns:
+    Dictionary of node properties
+    '''
+    return {
+        'position': position,
+        'g': g,
+        'h': h,
+        'f': g+h,
+        'parent': parent
+    }
+
+def euclidean_distance(pos_1: tuple[int, int], pos_2: tuple[int, int]) -> float:
+    '''
+    Calculate euclidean distance between two 2D coordinates
+    '''
+
+    x1, y1 = pos_1
+    x2, y2 = pos_2
+    return np.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+def construct_path(goal_node: dict):
+    '''
+    Returns path to goal_node from start as list of positions (x,y)
+    '''
+
+    # path starts empty, tracing back from goal node
+    path = []
+    current_node = goal_node
+
+    # iterate through parents until there aren't any
+    while current_node is not None:
+        path.append(current_node['position'])
+        current_node = current_node['parent']
+
+    # path is from goal to start, so reverse and return
+    return path[::-1]
+
+def get_neighbors(grid: np.ndarray, position: tuple[int, int]) -> list[tuple[int, int]]:
+    '''
+    Checks occupancy grid and returns unoccupied, unreached adjacent nodes
+
+    Keyword Arguments:
+    grid -- grid -- 2D numpy array occupancy grid, 0 = free 1 = obstacle; note (row, col) = (y, x)
+    position -- integer pair coordinates (x,y) of node to extend
+
+    Returns:
+    List of integer pair coordinates (x,y) of valid neighbors
+    '''
+
+    x, y = position
+    rows, cols = grid.shape
+
+    # all possible adjacent positions
+    adj_pos = [
+        (x+1, y), (x-1, y), # E, W
+        (x, y+1), (x, y-1), # N, S
+        (x+1, y+1), (x-1, y+1), # NE, NW
+        (x+1, y-1), (x-1, y-1), # SE, SW
+    ]
+
+    return [
+        (nx, ny) for nx, ny in adj_pos
+        if 0 <=nx < cols and 0 <= ny < rows # within grid
+        and grid[ny, nx] == 0 # location unoccupied
+    ]
+
+def show_path(grid: np.ndarray, path: list[tuple[int, int]]):
+    '''
+    Plot occupancy grid of obstacles and planned path, with start and goal positiions marked
+    note grid (row, col) = (y, x)
+    '''
+
+    # create figure and show grid
+    plt.figure(figsize=(10, 10))
+    plt.imshow(grid, cmap='binary')
+
+    # plot path and markers
+    if path:
+        path_arr = np.array(path)
+        plt.plot(path_arr[:,0], path_arr[:,1], 'b-', linewidth=3, label='Path')
+        plt.plot(path_arr[0,0], path_arr[0,1], 'go', markersize=15, label='Start')
+        plt.plot(path_arr[-1,0], path_arr[-1,1], 'ro', markersize=15, label='Goal')
+
+    plt.grid(True)
+    plt.legend(fontsize=12)
+    plt.title('A* Path Planning')
+    plt.show()
+
+# test and visualize
+# create empty grid
+grid = np.zeros((20,30))
+
+# add obstacles
+grid[10, 5:25] = 1 # E-W wall
+
+# start and goal positions
+start_pos = (4, 2)
+goal_pos = (28, 18)
+
+# find path
+path = a_star(grid=grid, start_pos=start_pos, goal_pos=goal_pos)
+
+# display if found
+if path:
+    show_path(grid=grid, path=path)
+else:
+    print("No path found.")
---
+++ lifetime_a.py
@@ -0,0 +1,249 @@
+import numpy as np
+import heapq
+import matplotlib.pyplot as plt
+
+## Lifetime Planning A*
+# structure: Graph contains all vertices keyed by their position and start/goal information. Grid is an occupancy grid of obstacles. 
+# Queue is a heap list of (key, position) tuple pairs.
+
+def calculate_key(vertex: dict, goal_pos: tuple[int, int]) -> tuple[float, float]:
+    '''
+    Calculates the key of a vertex on the graph.
+
+    Keyword Arguments:
+    vertex -- the vertex whose key is being calculated
+    start_pos -- the start position of the graph, (x,y)
+
+    Returns:
+    Key pair for priority queue
+    '''
+    return min(vertex['g'], vertex['rhs']) + heuristic(vertex['position'], goal_pos), min(vertex['g'], vertex['rhs'])
+
+def heuristic(pos1: tuple[int, int], pos2: tuple[int, int]) -> float:
+    '''
+    Calculates the heuristic between two positions on the graph, in this case
+    the Euclidean distance.
+    '''
+    x1, y1 = pos1
+    x2, y2 = pos2
+    return np.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+def update_vertex(vertex: dict, graph: dict, queue: list):
+    '''
+    Updates the rhs and key of a vertex
+    '''
+    if vertex['position'] != graph['start_pos']:
+        # update look-ahead distance estimate
+        rhs_min = float('inf')
+        for neighbor_pos in vertex['neighbors']:
+            neighbor = graph[neighbor_pos]
+            rhs_min = min(rhs_min, neighbor['g'] + heuristic(neighbor['position'], vertex['position']))
+        vertex['rhs'] = rhs_min
+
+    # remove from queue if present
+    in_queue = [item for item in queue if vertex['position'] in item] # weird fanagling to grab vertex with matching position from key queue
+    if in_queue != []:
+        queue.remove(in_queue[0])
+
+    # add back to queue if inconsistent
+    if vertex['g'] != vertex['rhs']:
+        vertex['key'] = calculate_key(vertex=vertex, goal_pos=graph['goal_pos'])
+        heapq.heappush(queue, (vertex['key'], vertex['position']))
+        
+def compute_shortest_path(queue: list, graph: dict) -> list[tuple[int, int]]:
+    '''
+    Computes and returns shortest path between start and goal.
+    '''
+    # loop while goal is inconsistent or there's a more efficient path
+    while queue[0][0] < calculate_key(vertex=graph[graph['goal_pos']], goal_pos=graph['goal_pos']) or \
+        graph[graph['goal_pos']]['g'] != graph[graph['goal_pos']]['rhs']:
+        
+        _, current_pos = heapq.heappop(queue)
+        
+        # if overconsistent, make consistent and update neighbors
+        if graph[current_pos]['g'] > graph[current_pos]['rhs']:
+            graph[current_pos]['g'] = graph[current_pos]['rhs']
+            for neighbor_pos in graph[current_pos]['neighbors']:
+                update_vertex(
+                    vertex = graph[neighbor_pos], 
+                    graph = graph, 
+                    queue = queue
+                )
+        
+        # otherwise, mark inf cost and update self and neighbors
+        else:
+            graph[current_pos]['g'] = float('inf')
+            for neighbor_pos in graph[current_pos]['neighbors']:
+                update_vertex(
+                    vertex = graph[neighbor_pos], 
+                    graph = graph, 
+                    queue = queue
+                )
+            update_vertex(
+                vertex = graph[current_pos],
+                graph = graph,
+                queue = queue
+            )
+    
+    # trace and return path
+    if graph[graph['goal_pos']]['g'] == float('inf'): return [] # empty if no path found
+    
+    else:
+        current_pos = graph['goal_pos']
+        path = [current_pos]
+
+        while current_pos != graph['start_pos']:
+            cost_min = float('inf')
+            pos_min = current_pos
+
+            # find neighbor with lowest path cost
+            for neighbor_pos in graph[current_pos]['neighbors']:
+                if graph[neighbor_pos]['g'] + heuristic(neighbor_pos, current_pos) < cost_min:
+                    cost_min = graph[neighbor_pos]['g'] + heuristic(neighbor_pos, current_pos)
+                    pos_min = neighbor_pos
+            
+            # append to path and iterate
+            path.append(pos_min)
+            current_pos = pos_min
+
+        return path
+    
+def get_neighbors(grid: np.ndarray, position: tuple[int, int]) -> list[tuple[int, int]]:
+    '''
+    Checks occupancy grid and returns unoccupied adjacent nodes
+
+    Keyword Arguments:
+    grid -- 2D numpy array occupancy grid, 0 = free 1 = obstacle; note (row, col) = (y, x)
+    position -- integer pair coordinates (x,y) of node to extend
+
+    Returns:
+    List of integer pair coordinates (x,y) of valid neighbors
+    '''
+
+    x, y = position
+    rows, cols = grid.shape
+
+    # all possible adjacent positions
+    adj_pos = [
+        (x+1, y), (x-1, y), # E, W
+        (x, y+1), (x, y-1), # N, S
+        (x+1, y+1), (x-1, y+1), # NE, NW
+        (x+1, y-1), (x-1, y-1), # SE, SW
+    ]
+
+    return [
+        (nx, ny) for nx, ny in adj_pos
+        if 0 <=nx < cols and 0 <= ny < rows # within grid
+        and grid[ny, nx] == 0 # location unoccupied
+    ]
+
+def create_vertex(position: tuple[int, int], g = float('inf'), rhs = float('inf'), neighbors = None) -> dict:
+    '''
+    Create a vertex with the given properties
+    '''
+    return {
+        'position': position,
+        'g': g,
+        'rhs': rhs,
+        'key': (float('inf'), float('inf')),
+        'neighbors': neighbors
+    }
+
+def create_graph(grid: np.ndarray, start_pos: tuple[int, int], goal_pos: tuple[int, int]) -> dict:
+    '''
+    Creates the graph containing all vertices of the state space
+    '''
+    rows, cols = grid.shape
+    graph = {}
+
+    # populate graph with default vertices
+    for nx in range(cols):
+        for ny in range(rows):
+            graph[(nx, ny)] = create_vertex(
+                position = (nx, ny),
+                neighbors = get_neighbors(grid=grid, position=(nx, ny))
+            )
+
+    # remember start and goal positions
+    graph['start_pos'] = start_pos
+    graph['goal_pos'] = goal_pos
+
+    return graph
+
+def show_path(grid: np.ndarray, path: list[tuple[int, int]]):
+    '''
+    Plot occupancy grid of obstacles and planned path, with start and goal positiions marked
+    note grid (row, col) = (y, x)
+    '''
+
+    # create figure and show grid
+    plt.figure(figsize=(10, 10))
+    plt.imshow(grid, cmap='binary')
+
+    # plot path and markers
+    if path:
+        path_arr = np.array(path)
+        plt.plot(path_arr[:,0], path_arr[:,1], 'b-', linewidth=3, label='Path')
+        plt.plot(path_arr[0,0], path_arr[0,1], 'go', markersize=15, label='Start')
+        plt.plot(path_arr[-1,0], path_arr[-1,1], 'ro', markersize=15, label='Goal')
+
+    plt.grid(True)
+    plt.legend(fontsize=12)
+    plt.title('LPA* Path Planning')
+    plt.show()
+
+def create_obstacle(grid: np.ndarray, coords: list[tuple[int, int]]):
+    '''
+    Given an occupancy grid and list of coordinates (x,y), mark the vertices at each coordinate
+    as occupied.
+
+    Keyword Arguments:
+    grid -- occupancy grid (row, col) = (y, x); 0 = empty, 1 = obstacle
+    '''
+
+## initialization
+# obstacle grid
+grid = np.zeros((20 ,30))
+grid[10, 5:25] = 1 # horizontal wall
+
+# start and goal positions
+start_pos = (4, 2)
+goal_pos = (28, 18)
+
+# graph of all vertices
+graph = create_graph(
+    grid = grid,
+    start_pos = start_pos,
+    goal_pos = goal_pos
+)
+
+# set start vertex rhs to zero
+graph[graph['start_pos']]['rhs'] = 0
+
+# create queue and add start vertex
+queue = [(calculate_key(graph[graph['start_pos']], graph['goal_pos']), graph['start_pos'])]
+
+## main path planning loop
+
+# forever
+    # compute_shortest_path
+    # wait for changes in edge costs
+    # for all edges with changed costs
+        # update edge cost
+        # update vertex
+
+
+## testing and visualization
+# find base path
+path = compute_shortest_path(queue=queue, graph=graph)
+
+# display if found
+if path:
+    show_path(grid=grid, path=path)
+else:
+    print("No path found.")
+
+# add new obstacle
+grid[5:10, 13] # vertical wall
+
+# 'update edge costs' in this implementation = update valid neighbors
